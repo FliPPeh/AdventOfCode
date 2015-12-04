e0f40ba87502b57e62669ab4d0faf3b7957d0df9
Lukas Niederbremer <Lukas.Niederbremer@gmail.com>
2015-12-04 19:52:32 +0100

    Unstable features all up in this

--- Day1/Rust/day1.rs
+++ Day1/Rust/day1.rs
@@ -1,34 +1,37 @@
+#![feature(io)]
+
 use std::io::{Read};
 
 fn main() {
-    let mut buf: [u8; 1] = [0; 1];
     let mut level: i32 = 0;
     let mut basement: Option<u32> = None;
     let mut i: u32 = 0;
 
-    while let Ok(n) = std::io::stdin().read(&mut buf) {
+    for c in std::io::stdin().chars() {
         i += 1;
 
-        if n == 0 {
-            break;
-        } else {
-            match std::char::from_u32(buf[0] as u32) {
-                Some('(') => {
-                    level += 1
-                },
-                Some(')') => {
-                    level -= 1;
+        match c.ok() {
+            Some('(') => {
+                level += 1
+            },
+            Some(')') => {
+                level -= 1;
 
-                    if level < 0 && basement.is_none() {
-                        basement = Some(i);
-                    }
-                },
-
-                // Newlines
-                Some('\n') | Some('\r') => {
-                    continue;
+                if level < 0 && basement.is_none() {
+                    basement = Some(i);
                 }
-                _  => { panic!("invalid input: {}", buf[0]) }
+            },
+
+            Some('\n') | Some('\r') => {
+                continue;
+            }
+
+            Some(x) => {
+                panic!("bad command: {}", x);
+            }
+
+            None => {
+                panic!("invalid input :(!");
             }
         }
     }
